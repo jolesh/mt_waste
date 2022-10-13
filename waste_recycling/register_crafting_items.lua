db5236fb00768bbc49d3439fba0e4b6392d396cd
jolesh <joraloe@gmail.com>
2022-10-13 22:40:28 +0200

    Changed to nuggets instead of simple recycle
    Added more clean nugget integration

--- waste_recycling/register_crafting_items.lua
+++ waste_recycling/register_crafting_items.lua
@@ -14,34 +14,3 @@ minetest.register_craftitem("waste_recycling:magnet", {
    description = S("Magnet"),
    inventory_image = "waste_recycling_magnet.png", --missing
 })]]
-
---Metal Nuggets
-
-if minetest.get_modpath("simple_recycle") then
-	return
-else
-	minetest.register_craftitem(":simple_recycle:steel_nugget", {
-	   description = S("Steel Nugget"),
-	   inventory_image = "simple_recycle_steel_nugget.png"
-	})
-
-	minetest.register_craftitem(":simple_recycle:bronze_nugget", {
-	   description = S("Bronze Nugget"),
-	   inventory_image = "simple_recycle_bronze_nugget.png"
-	})
-
-	minetest.register_craftitem(":simple_recycle:gold_nugget", {
-	   description = S("Gold Nugget"),
-	   inventory_image = "simple_recycle_gold_nugget.png"
-	})
-
-	minetest.register_craftitem(":simple_recycle:tin_nugget", {
-	   description = S("Tin Nugget"),
-	   inventory_image = "simple_recycle_tin_nugget.png"
-	})
-
-	minetest.register_craftitem(":simple_recycle:copper_nugget", {
-	   description = S("Copper Nugget"),
-	   inventory_image = "simple_recycle_copper_nugget.png"
-	})
-end
--- waste_recycling/register_recipes.lua
+++ waste_recycling/register_recipes.lua
@@ -8,25 +8,25 @@ minetest.register_craft({
 
 minetest.register_craft({
    type = "cooking",
-   output = "simple_recycle:tin_nugget",
+   output = "nuggets:tin_nugget",
    recipe = "waste_materials:tin_powder",
 })
 
 minetest.register_craft({
    type = "cooking",
-   output = "simple_recycle:copper_nuggett",
+   output = "nuggets:copper_nuggett",
    recipe = "waste_materials:copper_powder",
 })
 
 minetest.register_craft({
    type = "cooking",
-   output = "simple_recycle:steel_nugget",
+   output = "nuggets:steel_nugget",
    recipe = "waste_materials:steel_powder",
 })
 
 minetest.register_craft({
    type = "cooking",
-   output = "simple_recycle:gold_nugget",
+   output = "nuggets:gold_nugget",
    recipe = "waste_materials:gold_powder",
 })
 
@@ -34,28 +34,28 @@ minetest.register_craft({
 
 minetest.register_craft({
    type = "cooking",
-   output = "simple_recycle:steel_nugget",
+   output = "nuggets:steel_nugget",
    recipe = "waste_materials:waste_steel_object",
 	cooktime = 2,
 })
 
 minetest.register_craft({
    type = "cooking",
-   output = "simple_recycle:gold_nugget",
+   output = "nuggets:gold_nugget",
    recipe = "waste_materials:waste_gold_object",
 	cooktime = 2,
 })
 
 minetest.register_craft({
    type = "cooking",
-   output = "simple_recycle:copper_nugget",
+   output = "nuggets:copper_nugget",
    recipe = "waste_materials:waste_copper_object",
 	cooktime = 2,
 })
 
 minetest.register_craft({
    type = "cooking",
-   output = "simple_recycle:tin_nugget",
+   output = "nuggets:tin_nugget",
    recipe = "waste_materials:waste_tin_object",
 	cooktime = 2,
 })
@@ -68,104 +68,7 @@ minetest.register_craft({
 
 minetest.register_craft({
    type = "shapeless",
-   output = "default:diamond",
+   output = "nuggets:diamond_shard",
    recipe = {"waste_materials:waste_diamond_object"},
 	replacements = {{"waste_materials:waste_diamond_object", "waste_materials:waste_gold_object"}},
 })
-
---Crafting
-
----Nuggets
-
-if minetest.get_modpath("simple_recycle") then
-	return
-else
-
--- to nugget
-
-	minetest.register_craft({
-	   type = "shapeless",
-	   output = "simple_recycle:steel_nugget 9",
-	   recipe = {"default:steel_ingot"},
-	})
-
-	minetest.register_craft({
-	   type = "shapeless",
-	   output = "simple_recycle:bronze_nugget 9",
-	   recipe = {"default:bronze_ingot"},
-	})
-
-	minetest.register_craft({
-	   type = "shapeless",
-	   output = "simple_recycle:gold_nugget 9",
-	   recipe = {"default:gold_ingot"},
-	})
-
-	minetest.register_craft({
-	   type = "shapeless",
-	   output = "simple_recycle:tin_nugget 9",
-	   recipe = {"default:tin_ingot"},
-	})
-
-	minetest.register_craft({
-	   type = "shapeless",
-	   output = "simple_recycle:copper_nugget 9",
-	   recipe = {"default:copper_ingot"},
-	})
-
-	--to ingot
-
-	minetest.register_craft({
-	   output = "default:steel_ingot",
-	   recipe = {
-	      {"simple_recycle:steel_nugget", "simple_recycle:steel_nugget", "simple_recycle:steel_nugget"},
-	      {"simple_recycle:steel_nugget", "simple_recycle:steel_nugget", "simple_recycle:steel_nugget"},
-	      {"simple_recycle:steel_nugget", "simple_recycle:steel_nugget", "simple_recycle:steel_nugget"}
-	   }
-	})
-
-	minetest.register_craft({
-	   output = "default:bronze_ingot",
-	   recipe = {
-	      {"simple_recycle:bronze_nugget", "simple_recycle:bronze_nugget", "simple_recycle:bronze_nugget"},
-	      {"simple_recycle:bronze_nugget", "simple_recycle:bronze_nugget", "simple_recycle:bronze_nugget"},
-	      {"simple_recycle:bronze_nugget", "simple_recycle:bronze_nugget", "simple_recycle:bronze_nugget"}
-	   }
-	})
-
-	minetest.register_craft({
-	   output = "default:gold_ingot",
-	   recipe = {
-	      {"simple_recycle:gold_nugget", "simple_recycle:gold_nugget", "simple_recycle:gold_nugget"},
-	      {"simple_recycle:gold_nugget", "simple_recycle:gold_nugget", "simple_recycle:gold_nugget"},
-	      {"simple_recycle:gold_nugget", "simple_recycle:gold_nugget", "simple_recycle:gold_nugget"}
-	   }
-	})
-
-	minetest.register_craft({
-	   output = "default:tin_ingot",
-	   recipe = {
-	      {"simple_recycle:tin_nugget", "simple_recycle:tin_nugget", "simple_recycle:tin_nugget"},
-	      {"simple_recycle:tin_nugget", "simple_recycle:tin_nugget", "simple_recycle:tin_nugget"},
-	      {"simple_recycle:tin_nugget", "simple_recycle:tin_nugget", "simple_recycle:tin_nugget"}
-	   }
-	})
-
-	minetest.register_craft({
-	   output = "default:copper_ingot",
-	   recipe = {
-	      {"simple_recycle:copper_nugget", "simple_recycle:copper_nugget", "simple_recycle:copper_nugget"},
-	      {"simple_recycle:copper_nugget", "simple_recycle:copper_nugget", "simple_recycle:copper_nugget"},
-	      {"simple_recycle:copper_nugget", "simple_recycle:copper_nugget", "simple_recycle:copper_nugget"}
-	   }
-	})
-
-	minetest.register_craft({
-	   output = "default:bronze_ingot",
-	   recipe = {
-	      {"simple_recycle:copper_nugget", "simple_recycle:copper_nugget", "simple_recycle:copper_nugget"},
-	      {"simple_recycle:copper_nugget", "simple_recycle:tin_nugget", "simple_recycle:copper_nugget"},
-	      {"simple_recycle:copper_nugget", "simple_recycle:copper_nugget", "simple_recycle:copper_nugget"}
-	   }
-	})
-end
